66c79d830e265417b53ecd3011aa46ece6cd86d9
Subhajit <shubhajit.pramanik@edudigm.in>
2024-03-30 18:08:37 +0530

    Code commited for first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+//JS
+<script setup>
+import { ref } from 'vue';
+import HelloComponent from './components/HelloComponent.vue';
+let count = ref(0);
+
+const incriment = (v) => {
+  count.value += v;
+};
+const dicriment = (v) => {
+  count.value -= v;
+};
+
+</script>
+
+// HTML
+<template>
+  <HelloComponent />
+  <!-- Dinamic Binding -->
+    <!-- <a v-bind:href="link">Google</a> -->
+    <a :href="link">Google</a>
+    <h2>Count {{ count }}</h2>
+    <!-- Event Binding -->
+    <!-- <button v-on:click="incriment(2)">Incriment</button> -->
+    <button @:click="incriment(2)">Incriment</button>
+    <button @click="dicriment(1)">Dicriment</button>
+</template>
+
+// CSS
+<style scoped>
+
+</style>
---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloAnother from './HelloAnother.vue';
+const link = "https://www.google.com";
+</script>
+
+<template>
+    <h1>Hello World</h1>
+    <HelloAnother />
+</template>
